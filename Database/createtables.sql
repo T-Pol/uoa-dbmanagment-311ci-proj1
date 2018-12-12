d9412ef9a35ac11b287c9f4d1847693a32277b66
T-Pol <theodopol@gmail.com>
2018-12-12 13:36:37 +0200

    Create createtables.sql

---
+++ Database/createtables.sql
@@ -0,0 +1,8 @@
+-- -----------------------------------------------------
+-- Table status
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS status 
+(
+  status_id VARCHAR(15) NOT NULL,
+  PRIMARY KEY (status_id)
+)
