97b17d1ff56b24b813bad4785d8853f523de95ff
T-Pol <theodopol@gmail.com>
2018-12-12 15:42:46 +0200

    Update createtables.sql

--- Database/createtables.sql
+++ Database/createtables.sql
@@ -161,4 +161,16 @@ CREATE TABLE tree_trims
 )
 
 
+--data for users
 
+CREATE TABLE users
+(
+  id serial NOT NULL,
+  name character varying(255) NOT NULL,
+  username character varying(255) NOT NULL,
+  email character varying(255) NOT NULL,
+  password character varying(255) NOT NULL,
+  address character varying(255) NOT NULL,
+  role integer,
+  PRIMARY KEY (id)
+)
