a9f84af380bc1641a9ac71c8970e2d0576ea8a65
T-Pol <theodopol@gmail.com>
2018-12-12 13:38:15 +0200

    Update createtables.sql

--- Database/createtables.sql
+++ Database/createtables.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS status
   status_id VARCHAR(15) NOT NULL,
   PRIMARY KEY (status_id)
 )
+
+
+-- -----------------------------------------------------
+-- Table type_of_service_Request
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS type_of_Service_Request 
+(
+  idtype_of_service_request VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idtype_of_Service_Request)
+)
